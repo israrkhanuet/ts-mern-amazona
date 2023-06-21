68b2c82e1a7177f5fe86cd87aa9381fabb775dbe
israrkhanuet <israrkhanuet@gmail.com>
2023-06-21 18:43:01 +0500

    Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { productsSample } from "./data";
-import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
@@ -22,19 +22,7 @@ function App() {
         </header>
         <main>
           <Container className="mt-3">
-            <Row>
-              {productsSample.map((product) => (
-                <Col key={product.slug} sm={6} md={4} lg={3}>
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                    className="product-image"
-                  />
-                  <h2> {product.name}</h2>
-                  <p>${product.price}</p>
-                </Col>
-              ))}
-            </Row>
+            <Outlet />
           </Container>
         </main>
         <footer>
